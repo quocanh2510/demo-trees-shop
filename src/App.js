ea1483de5160373b5c1d48a6dac8e1df4a135a47
Your Name <you@example.com>
2022-11-22 10:14:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '~/pages/Home';
 import BlogPage from '~/pages/Blog';
 import AboutPage from '~/pages/About';
 import ContactPage from '~/pages/Contact';
-import LoadingPage from './components/LoadingPage';
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    setTimeout(() => {
-        setLoading(false);
-    }, 2000);
-
     return (
-        <>
-            {loading ? (
-                <LoadingPage />
-            ) : (
-                <Router>
-                    <div className="App">
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/blog" element={<BlogPage />} />
-                            <Route path="/about" element={<AboutPage />} />
-                            <Route path="/contact" element={<ContactPage />} />
-                        </Routes>
-                    </div>
-                </Router>
-            )}
-        </>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/blog" element={<BlogPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
